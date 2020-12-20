74fbe7e5074b410ced4f24cee085bf5cab94c709
aliyeysides <ali.yeysides@gmail.com>
2020-12-19 23:52:46 -0500

    adds certs data

---
+++ src/data/certificates-data.tsx
@@ -0,0 +1,22 @@
+import { Certificate } from "../CertificationsSection";
+
+export const CertsData: Certificate[] = [
+  {
+    name: "Machine Learning",
+    issuer: "Coursera",
+    issuedDate: new Date(2020, 10),
+    url: "https://www.google.com",
+  },
+  {
+    name: "AWS Fundamentals: Addressing Security Risk",
+    issuer: "Coursera",
+    issuedDate: new Date(2020, 11, 2),
+    url: "https://www.google.com",
+  },
+  {
+    name: "AWS Fundamentals: Going Cloud-Native",
+    issuer: "Coursera",
+    issuedDate: new Date(2020, 11, 1),
+    url: "https://www.google.com",
+  },
+];
